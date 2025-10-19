f1899053bbe535c2e309c59f040eaf8cc63268f3
thomas-f-allard <157734588+thomas-f-allard@users.noreply.github.com>
2025-10-19 18:13:44 +0000

    Deploying to gh-pages from @ thomas-f-allard/thomas-f-allard.github.io@215d6adffdc6dc74566350dbee001b094a74bde2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
@@ -51,14 +58,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/books/";
               },
-            },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "books-the-godfather",
+            },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
