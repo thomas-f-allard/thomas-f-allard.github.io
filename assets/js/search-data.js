52a89b9dd2efd70025bc8075850352cce28a5023
thomas-f-allard <157734588+thomas-f-allard@users.noreply.github.com>
2025-10-20 23:47:54 +0000

    Deploying to gh-pages from @ thomas-f-allard/thomas-f-allard.github.io@36affea14cd2c8c1805cc76264365ba99a21d1cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum-vitæ",
+          title: "curriculum vitæ",
+          description: "A full CV is available [here](https://thomas-f-allard.github.io/assets/pdf/example_pdf.pdf)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
