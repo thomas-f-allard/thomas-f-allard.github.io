1e9220d2406d387e3fdbe90255123fdc4786980e
thomas-f-allard <157734588+thomas-f-allard@users.noreply.github.com>
2025-10-22 09:11:59 +0000

    Deploying to gh-pages from @ thomas-f-allard/thomas-f-allard.github.io@867d6ced865067fd3697d4c31f10f398687500cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
